73a3db507bc74c961b527f9467139645cc9925f0
judith carreño <3280kg@gmail.com>
2022-04-18 21:35:16 -0400

    primer commit

---
+++ js.js
@@ -0,0 +1,15 @@
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+
+let primera = Math.floor(Math.random() * 5);
+let segunda = Math.floor(Math.random() * 5);
+let tercera = Math.floor(Math.random() * 4);
+let cuarta = Math.floor(Math.random() * 6);
+
+console.log(who[primera]+""+action[segunda]+""+ what[tercera]+""+when[cuarta])
+
+
+
